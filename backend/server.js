7a0d89e6cb913b6cce1c75b5d13446b9c3f50486
Foo Pang <foo.pang@gmail.com>
2016-08-20 20:15:48 +0800

    Upgrade to firebase 3.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 var Queue = require('firebase-queue'),
-    Firebase = require('firebase'),
+    firebase = require('firebase'),
     Filter = require('bad-words');
 
-var filter = new Filter();
-var ref = new Firebase('https://sanichat.firebaseio.com/');
-ref.authWithCustomToken('<your-token-here>', function(err, authData){
-    if (err) {
-      console.log("Login failed with error: ", err);
-    } else {
-      console.log("Authenticated successfully with payload: ", authData);
-    }
+firebase.initializeApp({
+    apiKey: "AIzaSyCrQYWZ1yqx0mKtztAU4Rp63HAmFq5mDzI",
+    databaseURL: "https://sanichat-3abb4.firebaseio.com",
 });
-var queue = new Queue(ref, function(data, progress, resolve, reject) {
+
+var queueRef = firebase.database().ref('queue');
+var messagesRef = firebase.database().ref('messages');
+
+var filter = new Filter();
+
+var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
   data.message = filter.clean(data.message);
-  ref.child('messages').push(data, function(err){
+  console.log(data);
+  messagesRef.push(data, function(err){
     if (err) {
         reject(err);
     } else {
